956f5ed2af2619443ecf53c9f828aa8caf08ae09
kyuu <arattanasack@gmail.com>
2019-10-27 17:29:42 -0700

    Renders first question

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,71 +10,70 @@ const choiceD = $("#D");
 const counter = $("#counter");
 const timeGauge = $("#timeGauge");
 const progress = $("#progress");
-const score = $("#score");
+const scoreDiv = $("#score");
 
 //Questions
 let questions= [
-{
-    question:"In 45 A.D., this fruit was originally called “ida” after the mountain it was grown on.",
-    imgSrc:"",
-    ChoiceA:"Raspberry",
-    choiceB:"Orange",
-    choiceC:"Grape",
-    choiceD:"Pear",
-    correct:"Raspberry",
-},
-{
-    question:"",
-    imgSrc:"",
-    ChoiceA:"",
-    choiceB:"",
-    choiceC:"",
-    choiceD:"",
-    correct:"",
-},
-{
-    question:"",
-    imgSrc:"",
-    ChoiceA:"",
-    choiceB:"",
-    choiceC:"",
-    choiceD:"",
-    correct:"",
-},
-{
-    question:"",
-    imgSrc:"",
-    ChoiceA:"",
-    choiceB:"",
-    choiceC:"",
-    choiceD:"",
-    correct:"",
-},
-{
-    question:"",
-    imgSrc:"",
-    ChoiceA:"",
-    choiceB:"",
-    choiceC:"",
-    choiceD:"",
-    correct:"",
-}
-];
-
+    {
+        question:"In 45 A.D., this fruit was originally called “ida” after the mountain it was grown on.",
+        imgSrc:"https://andrewsfruitandproduce.com/files/2014/04/basket.png",
+        choiceA:"Raspberry",
+        choiceB:"Orange",
+        choiceC:"Grape",
+        choiceD:"Pear",
+        correct:"Raspberry",
+    },
+    
+    ];
+    
+//Variables
 const lastQuestion = questions.length - 1;
 let runningQuestion = 0;
+// let count = 0;
+// const questionTime = 10;
+// const gaugeWidth = 150;
+// const gaugeUnit = gaugeUnit / questionTime;
+// let timer;
+// let score = 0;
+
 
 //Render Questions
 const renderQuestion = () => {
     let q = questions[runningQuestion];
 
-    question.html = "<p>" + q.question + "</p>";
-    qImg.html = "<img src" + q.imgSrc + ">";
-    ChoiceA.html = q.choiceA;
-    ChoiceB.html = q.choiceB;
-    ChoiceC.html = q.choiceC;
-    ChoiceD.html = q.choiceD;
+    question.html("<p>" + q.question + "</p>");
+    qImg.html("<img src=" + q.imgSrc + ">");
+    choiceA.html(q.choiceA);
+    choiceB.html(q.choiceB);
+    choiceC.html(q.choiceC);
+    choiceD.html(q.choiceD);
+    console.log(q);
 }
 
+// const renderProgress = () => {
+//     for (let i = 0; i < lastQuestion; i++) {
+//         progress.html += "<div class='prog' id = " + i + "</div>";
+//     }
+// }
 
+// const renderCounter = () => {
+//     if(count <= questionTime){
+//         counter.html = count;
+//         timeGauge.css("width", count * gaugeUnit +"px");
+//         count++;
+//     }else{
+//         count = 0;
+//     }
+// }
+
+
+//Start the quiz
+$("#start").on("click", function(){
+    start.css("display", "none");
+    renderQuestion();
+    quiz.css("display", "block");
+    // renderProgress();
+    // renderCounter();
+    // timer = setInterval(renderCounter, 1000);
+});
 // renderQuestion();
