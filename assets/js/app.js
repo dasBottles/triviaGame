433bd5106a20d73bae91d9b3d7659899c8d023aa
David <arattanasack@gmail.com>
2020-03-06 00:43:38 -0800

    fixes functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,49 +1,48 @@
 //DOM Targets
-const start = $("#start");
-const quiz = $("#quiz");
-const question = $("#question");
-const qImg = $("#qImg");
-const choiceA = $("#A");
-const choiceB = $("#B");
-const choiceC = $("#C");
-const choiceD = $("#D");
-const counter = $("#counter");
-const timeGauge = $("#timeGauge");
-const progress = $("#progress");
-const scoreDiv = $("#score");
+const start = $("#start"),
+    quiz = $("#quiz"),
+    question = $("#question"),
+    qImg = $("#qImg"),
+    choiceA = $("#choiceA"),
+    choiceB = $("#choiceB"),
+    choiceC = $("#choiceC"),
+    choiceD = $("#choiceD"),
+    counter = $("#counter"),
+    timeGauge = $("#timeGauge"),
+    progress = $("#progress"),
+    scoreDiv = $("#score")
 
 //Questions
-let questions= [
-    {
-        question:"In 45 A.D., this fruit was originally called “ida” after the mountain it was grown on.",
-        imgSrc:"https://andrewsfruitandproduce.com/files/2014/04/basket.png",
-        choiceA:"Raspberry",
-        choiceB:"Orange",
-        choiceC:"Grape",
-        choiceD:"Pear",
-        correct:"Raspberry",
+let questions = [{
+        question: "In 45 A.D., this fruit was originally called “ida” after the mountain it was grown on.",
+        imgSrc: "https://andrewsfruitandproduce.com/files/2014/04/basket.png",
+        choiceA: "Raspberry",
+        choiceB: "Orange",
+        choiceC: "Grape",
+        choiceD: "Pear",
+        correct: "A",
     },
     {
-        question:"In the culinary world, what does MSG stand for?",
-        imgSrc:"https://www.thehealthy.com/wp-content/uploads/2018/08/What-is-MSG%E2%80%94And-How-Bad-Is-It-Really-760x506.jpg",
-        choiceA:"Massage",
-        choiceB:"Mmm, so good",
-        choiceC:"Monosodium glutamate",
-        choiceD:"Message",
-        correct:"Monosodium glutamate",
+        question: "In the culinary world, what does MSG stand for?",
+        imgSrc: "https://www.thehealthy.com/wp-content/uploads/2018/08/What-is-MSG%E2%80%94And-How-Bad-Is-It-Really-760x506.jpg",
+        choiceA: "Massage",
+        choiceB: "Mmm, so good",
+        choiceC: "Monosodium glutamate",
+        choiceD: "Message",
+        correct: "C",
     },
     {
-        question:"Who was the 35th president of the United States?",
-        imgSrc:"https://i.imgflip.com/12wxqp.jpg",
-        choiceA:"Abraham Lincoln",
-        choiceB:"John F. Kennedy",
-        choiceC:"Herbert Hoover",
-        choiceD:"Ronald Reagan",
-        correct:"John F. Kennedy",
+        question: "Who was the 35th president of the United States?",
+        imgSrc: "https://i.imgflip.com/12wxqp.jpg",
+        choiceA: "Abraham Lincoln",
+        choiceB: "John F. Kennedy",
+        choiceC: "Herbert Hoover",
+        choiceD: "Ronald Reagan",
+        correct: "B",
     },
-    
-    ];
-    
+
+];
+
 //Variables
 const lastQuestion = questions.length - 1;
 let runningQuestion = 0;
@@ -68,51 +67,54 @@ const renderQuestion = () => {
     console.log(q);
 }
 
-const renderProgress = () => {
-    for (let i = 0; i < lastQuestion; i++) {
-        progress.html += "<div class='prog' id = " + i + "</div>";
-    }
-}
 
 const renderCounter = () => {
-    if(count <= questionTime){
+    if (count <= questionTime) {
         counter.html = count;
-        timeGauge.css("width", count * gaugeUnit +"px");
+        timeGauge.css("width", count * gaugeUnit + "px");
         count++;
-    }else{
+    } else {
         count = 0;
+        if (runningQuestion < lastQuestion) {
+            runningQuestion++;
+            renderQuestion();
+        } else {
+            clearInterval(timer);
+            scoreRender();
+        }
     }
+
 }
 
 const checkAnswer = (answer) => {
- if(answer === question.correct){
-     score++;
-     answerCorrect();
- }else{
+    if (answer === questions[runningQuestion].correct) {
+        score++;
+    }
 
- }
- 
- count = 0;
- if(runningQuestion < lastQuestion){
-     runningQuestion++;
-     renderQuestion();
- }else{
-     clearInterval(timer);
-     scoreRender();
- }
+    count = 0;
+    if (runningQuestion < lastQuestion) {
+        runningQuestion++;
+        renderQuestion();
+    } else {
+        clearInterval(timer);
+        endQuiz();
+    }
+
+    console.log('hi this works')
 }
 
-const answerCorrect = () => {
-
+const endQuiz = () => {
+    const resultsDisplay = 
+    `<div>You got ${score} out of ${questions.length} right!</div>`
+    $('#quiz-container').addClass('resultsBackground')
+    $('#quiz').empty().html(resultsDisplay);
 }
 
 //Start the quiz
-$("#start").on("click", function(){
+$("#start").on("click", function () {
     start.css("display", "none");
     renderQuestion();
-    quiz.css("display", "block");
-    renderProgress();
+    quiz.css("display", "flex");
     renderCounter();
     timer = setInterval(renderCounter, 1000);
 });
-// renderQuestion();
